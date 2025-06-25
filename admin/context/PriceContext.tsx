a34405a06acbf1cd5d40894ccb961988eccc02c2
Daler <dsarifkulo021@gmail.com>
2025-06-25 17:45:55 +0500

    toaster integeated in a price hook

--- admin/context/PriceContext.tsx
+++ admin/context/PriceContext.tsx
@@ -1,7 +1,8 @@
 // app/components/PriceContext.tsx
 "use client";
 import { AuPrice } from "@/app/types";
-import { createContext, useCallback, useContext } from "react";
+import { createContext, useCallback, useContext, useEffect } from "react";
+import { toast } from "sonner";
 
 export const PriceContext = createContext<AuPrice | null>(null);
 
@@ -20,17 +21,26 @@ export function PriceProvider({
 export function usePrice() {
 	const AuPrice = useContext(PriceContext);
 
+	useEffect(() => {
+		if (!AuPrice) {
+			toast.error("Цены на золото временно недоступны", {
+				style: {
+					background: "red",
+					color: "#fff",
+					borderColor: "red",
+				},
+			});
+		}
+	}, [AuPrice]);
+
 	const calculate = useCallback(
 		({ weight, markup }: { weight: number; markup: number }) => {
-			console.count("calced");
+			if (!AuPrice) return 0;
 
-			if (!AuPrice) {
-				return 0;
-			}
-			const priceWithoutMarkup = Number(AuPrice?.pricePerGram) * weight;
-			const priceWithMarkup = priceWithoutMarkup * (1 + markup / 100);
+			const pricePerGram = Number(AuPrice.pricePerGram);
+			const priceWithoutMarkup = pricePerGram * weight;
 
-			return priceWithMarkup;
+			return priceWithoutMarkup * (1 + markup / 100);
 		},
 		[AuPrice]
 	);
