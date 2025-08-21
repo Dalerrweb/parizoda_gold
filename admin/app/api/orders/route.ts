ebb49ac4132b986af63f4f92245d572a48c7e7d9
ShukurovOzodbek <shukurov.ozodbek.jaloliddinovich@gmail.com>
2025-08-21 16:26:01 +0500

    -

--- admin/app/api/orders/route.ts
+++ admin/app/api/orders/route.ts
@@ -46,7 +46,10 @@ export async function POST(req: NextRequest) {
 		const auth = await requireAuth(req);
 		if (!auth.success) return auth.response;
 		const userId = auth.payload.userId;
+		console.info(userId);
+		
 		const user: any = await prisma.user.findUnique({ where: { telegramId: userId }, select: { id: true } });
+		console.info(user);
 
 		const body = await req.json();
 
